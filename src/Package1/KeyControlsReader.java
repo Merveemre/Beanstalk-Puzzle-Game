f197c7f309603e151b70cb940837763c40915efe
Merve Emre <129984815+Merveemre@users.noreply.github.com>
2025-07-09 19:42:34 +0200

    Update KeyControlsReader.java

--- src/Package1/KeyControlsReader.java
+++ src/Package1/KeyControlsReader.java
@@ -39,7 +39,7 @@ public class KeyControlsReader {
     }
 
     private void loadControlsFromFile() {
-        String filePath = "C:\\BeansTalkGame\\src\\key_controls.txt";
+        String filePath = "C:\\Desktop\\BeanstalkGame\\src\\character_symbols.txt";
 
         try {
             File file = new File(filePath);
